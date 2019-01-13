910f44094587833dc4231819f156b05d2d31abaf
piyushimraw <piyush.srivastavaimraw@gmail.com>
2019-01-13 16:29:02 +0530

    Added Prettier

--- src/app.ts
+++ src/app.ts
@@ -1,20 +1,20 @@
-import * as bodyParser from 'body-parser';
-import * as express from 'express';
-import { Routes } from './routes/routes';
+import * as bodyParser from 'body-parser'
+import * as express from 'express'
+import { Routes } from './routes/routes'
 
 class App {
-    public app: express.Application;
-    public routePrv: Routes =  new Routes();
+    public app: express.Application
+    public routePrv: Routes = new Routes()
 
     constructor() {
-        this.app = express();
-        this.routePrv.routes(this.app);
-        this.config();
+        this.app = express()
+        this.routePrv.routes(this.app)
+        this.config()
     }
     private config(): void {
-        this.app.use(bodyParser.json());
-        this.app.use(bodyParser.urlencoded({extended: false}));
+        this.app.use(bodyParser.json())
+        this.app.use(bodyParser.urlencoded({ extended: false }))
     }
 }
 
-export default new App().app;
+export default new App().app
