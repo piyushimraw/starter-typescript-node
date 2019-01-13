21a406c259d43dc4be59bcbf0ceab280430febf7
piyushimraw <piyush.srivastavaimraw@gmail.com>
2019-01-13 16:30:31 +0530

    Testing Prettier

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -1,24 +1,23 @@
-import {Request, Response} from 'express';
+import { Request, Response } from 'express'
 
 export class Routes {
     public routes(app): void {
-        app.route('/')
-            .get((req: Request, res: Response) => {
-                res.status(200).send({
-                    message: 'GET request Succesfull!!!',
-                });
-            });
+        app.route('/').get((req: Request, res: Response) => {
+            res.status(200).send({
+                message: 'GET request Succesfull!!!',
+            })
+        })
         // Get for Contacts
         app.route('/contacts')
             .get((req: Request, res: Response) => {
                 res.status(200).send({
                     message: 'GET Contacts Successfull !!',
-                });
+                })
             })
             .post((req: Request, res: Response) => {
                 res.status(200).send({
                     message: 'POST Contacts Successfull',
-                });
-            });
-        }
+                })
+            })
+    }
 }
